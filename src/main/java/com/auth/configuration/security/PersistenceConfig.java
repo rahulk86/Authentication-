0d8fe0857c11c6345289e9604098ef8e7c996655
rahulk86 <rahulkumarcodinggfg@gmail.com>
2024-02-25 23:28:23 +0530

    -- first commit

--- src/main/java/com/auth/configuration/security/PersistenceConfig.java
+++ src/main/java/com/auth/configuration/security/PersistenceConfig.java
@@ -1,19 +1,15 @@
-package com.auth.security;
+package com.auth.configuration.security;
 
 import com.auth.modal.user.User;
-import com.auth.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.auth.security.AuditorAwareImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.core.Authentication;
 
 @Configuration
 public class PersistenceConfig {
-    @Autowired
-    private UserService userService;
     @Bean
-    AuditorAware<User> auditorProvider(Authentication authentication) {
-        return new AuditorAwareImpl(userService,authentication);
+    AuditorAware<User> auditorProvider() {
+        return new AuditorAwareImpl();
     }
 }
--- src/main/java/com/auth/security/AuditorAwareImpl.java
+++ src/main/java/com/auth/security/AuditorAwareImpl.java
@@ -1,2 +1,36 @@
-package com.auth.security;public class AuditorAwareImpl {
+package com.auth.security;
+
+import com.auth.dto.UserDto;
+import com.auth.modal.user.User;
+import com.auth.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<User> {
+    @Autowired
+    private UserService userService;
+    @Override
+    @NonNull
+    public Optional<User> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+
+        Object principal = authentication.getPrincipal();
+        User user =null;
+        if (principal instanceof UserDto) {
+            user =  userService.toUser((UserDto) principal);
+        }
+        if(user==null){
+          return Optional.empty();
+        }
+        return Optional.of(user);
+    }
 }
